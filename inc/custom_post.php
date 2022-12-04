f1ca8a88160dd1169f3553309ed6e629ed9e0ffb
Mehedi <personal.mehedi@outlook.com>
2022-12-04 17:37:51 +0600

    slider add

--- inc/custom_post.php
+++ inc/custom_post.php
@@ -1,5 +1,38 @@
-<?php
 
+
+<?php
+//slider add option
+function custom_slider(){
+    register_post_type ('slider',
+      array(
+        'labels' => array(
+          'name' => ('Slider'),
+          'singular_name' => ('Slider'),
+          'add_new' => ('Add New Slider'),
+          'add_new_item' => ('Add New Slider'),
+          'edit_item' => ('Edit Slider'),
+          'new_item' => ('New Slider'),
+          'view_item' => ('View Slider'),
+          'not_found' => ('Sorry, we cound\'n find the Slider you are looking for.'),
+        ),
+        'menu_icon' => 'dashicons-format-gallery',
+        'public' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => true,
+        'menu_position' => 5, 
+        'has_archive' => true,
+        'hierarchial' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'rewrite' => array('slag' => 'slider'),
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+        )
+      );
+      add_theme_support('post-thumbnails');
+  }
+  
+  add_action('init', 'custom_slider');
+//custom Post register & service addd
 function custom_service(){
     register_post_type('service', 
      array(
@@ -8,9 +41,11 @@ function custom_service(){
             'singular_name'=>('Service'),
             'add_new'=> ('Add New Service '),
             'add_new_item'=> ('Add New Service '),
+            'edit_item'=> ('Edit Service'),
+            'view_item'=> ('View Slide'),
             'view_item'=> ('View Service'),
             'not_found'=>('Sorry, we can not found any servise that you looking for'),
-        ),
+                        ),
         'menu_icon'=>'dashicons-hammer',
         'public'=>true,
         'publicaly_queryable'=> true,
@@ -25,6 +60,13 @@ function custom_service(){
 
      )
     );
+    add_theme_support('post_thumbnails');
 }
 
 add_action('init', 'custom_service');
+
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * bx slider active 
+ */
+
+ jQuery(document).ready(function($){
+   $('.slider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    
+   });
+  });
